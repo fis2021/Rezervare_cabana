b09ca0036e494fd0d871ec1030817422e4a84ba4
Melkor55 <61756987+Melkor55@users.noreply.github.com>
2021-05-11 04:59:17 +0300

    added 'Create Ad' interface

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -5,7 +5,6 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import org.loose.fis.sre.exceptions.AccountDoesntExistException;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.sre.services.UserService;
 
