0cc2621e0d5f26a01a58407f243f43718d289870
Melkor55 <61756987+Melkor55@users.noreply.github.com>
2021-05-12 20:12:49 +0300

    Interface for the Rental Process added

---
+++ src/main/java/org/loose/fis/sre/controllers/RentController.java
@@ -0,0 +1,25 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.loose.fis.sre.exceptions.AdAlreadyExistsException;
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.sre.services.AdService;
+import org.loose.fis.sre.services.UserService;
+
+public class RentController
+{
+    public void handleRentingAction(MouseEvent mouseEvent)
+    {
+
+    }
+}
