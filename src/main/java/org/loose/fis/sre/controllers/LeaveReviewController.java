b7f4a0d1c9830950c1840167ddb9c1dd3f4e2300
Melkor55 <61756987+Melkor55@users.noreply.github.com>
2021-05-13 05:11:42 +0300

    Leave Review feature added

---
+++ src/main/java/org/loose/fis/sre/controllers/LeaveReviewController.java
@@ -0,0 +1,10 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.scene.input.MouseEvent;
+
+public class LeaveReviewController {
+    public void handleLeaveReviewAction(MouseEvent mouseEvent)
+    {
+
+    }
+}
