9acc325fe937e322d8a93ae919ebad330c348cd9
Melkor55 <61756987+Melkor55@users.noreply.github.com>
2021-05-15 01:45:31 +0300

    Cabana13,Cabana15,Cabana31,Cabana33 updated, added the feature of viewing newly rented properties by the Client in ClientHome page

--- src/main/java/org/loose/fis/sre/controllers/ClientHomeController.java
+++ src/main/java/org/loose/fis/sre/controllers/ClientHomeController.java
@@ -1,6 +1,5 @@
 package org.loose.fis.sre.controllers;
 
-import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +11,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.RemoveOptions;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.dizitart.no2.objects.filters.ObjectFilters;
 import org.loose.fis.sre.model.Ad;
@@ -23,10 +22,10 @@ import org.loose.fis.sre.services.RenterService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
-import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+import static org.dizitart.no2.objects.filters.ObjectFilters.and;
+import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
 
 public class ClientHomeController extends AdService implements Initializable {
 
@@ -54,8 +53,18 @@ public class ClientHomeController extends AdService implements Initializable {
         //
 
         stage.setTitle("Search to Rent");
-        stage.setScene(new Scene(root, 850, 700));
-        stage.show();
+        stage.setScene(new Scene(root, 850, 900));
+        stage.showAndWait();
+        if(controller.checkIfPropertyRented2)
+        {
+            for (int i = 0 ; i < controller.returnAdRented.length ; i ++ )
+            {
+                if (controller.returnAdRented[i] != null)
+                tableView.getItems().add(controller.returnAdRented[i]);
+            }
+
+            tableView.refresh();
+        }
     }
 
     public void switchStage_to_Leave_Review() throws IOException
@@ -154,6 +163,7 @@ public class ClientHomeController extends AdService implements Initializable {
     }
 
     public static String property_name ;
+    //ObservableList<Ad> PropertiesRentedByClient = tableView.getItems();
     public void select()
     {
         Ad ad = tableView.getSelectionModel().getSelectedItem();
@@ -176,7 +186,12 @@ public class ClientHomeController extends AdService implements Initializable {
 
     public void handleCancelRentalAction(MouseEvent mouseEvent)
     {
-
+        select();
+        RemoveOptions options = new RemoveOptions();
+        options.setJustOne(true);
+        RenterRepository.remove(and(eq("nume_proprietate", property_name),eq("full_name",this.ClientName)),options);
+        tableView.getItems().remove(tableView.getSelectionModel().getSelectedItem());
+        tableView.refresh();
     }
 
     @Override
--- src/main/java/org/loose/fis/sre/controllers/RentController.java
+++ src/main/java/org/loose/fis/sre/controllers/RentController.java
@@ -1,14 +1,17 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.loose.fis.sre.exceptions.AdAlreadyExistsException;
 import org.loose.fis.sre.exceptions.NoPasswordException;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
@@ -20,10 +23,11 @@ import org.loose.fis.sre.services.RenterService;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class RentController extends RenterService implements Initializable
-{
+public class RentController extends RenterService implements Initializable {
     @FXML
     public TextField nume_proprietate;
+    @FXML
+    public HBox RentButton;
     @FXML
     private Text rentingMessage;
     @FXML
@@ -35,9 +39,18 @@ public class RentController extends RenterService implements Initializable
     @FXML
     private CheckBox over_18;
 
-    public void getClientNameText_as_Renter(String clientName)
-    {
+    public boolean checkIfPropertyRented = false;
+
+    public void getClientNameText_as_Renter(String clientName) {
         full_name.setText(clientName);
+    }
+
+    public void closeStageAfterRentButtonPressed()
+    {
+        Stage stage = (Stage) RentButton.getScene().getWindow();
+        PauseTransition delay = new PauseTransition(Duration.seconds(1));
+        delay.setOnFinished( event -> stage.close() );
+        delay.play();
     }
 
     public void handleRentingAction()
@@ -45,7 +58,9 @@ public class RentController extends RenterService implements Initializable
         try
         {
             RenterService.addRenter(nume_proprietate.getText(),full_name.getText(), email.getText(), phone.getText(), over_18.isSelected());
-            rentingMessage.setText("Renting process complete!");
+            rentingMessage.setText("Rental process complete!");
+            checkIfPropertyRented = true ;
+            closeStageAfterRentButtonPressed();
         }
         catch (RenterAlreadyExistsException e)
         {
