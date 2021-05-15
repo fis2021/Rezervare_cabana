c2bfd644fe7e1b2bd39b6a4756143375930f8b2d
Melkor55 <61756987+Melkor55@users.noreply.github.com>
2021-05-15 05:32:32 +0300

    Cabana18,Cabana12,Cabana31,Cabana32,Cabana33 updated, added some new functionalities to the buttons and new properties to some GUIs tables

--- src/main/java/org/loose/fis/sre/controllers/ReviewsController.java
+++ src/main/java/org/loose/fis/sre/controllers/ReviewsController.java
@@ -1,31 +1,135 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 import org.loose.fis.sre.exceptions.AdAlreadyExistsException;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.sre.model.Ad;
+import org.loose.fis.sre.model.Review;
 import org.loose.fis.sre.services.AdService;
 import org.loose.fis.sre.services.UserService;
 import javafx.scene.control.TextField;
 import org.loose.fis.sre.services.ReviewService;
 
-public class ReviewsController extends ReviewService
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReviewsController extends ReviewService implements Initializable
 {
     @FXML
-    public TextField nume_proprietate;
+    public TextField Nume_proprietate;
+    @FXML
+    public TableView<Review> table;
     @FXML
     public TextField author_name;
+    @FXML
+    public TableColumn Nr;
+    @FXML
+    public TableColumn Date;
+    @FXML
+    public TableColumn nume_autor;
+    @FXML
+    public TableColumn textReview;
+    @FXML
+    public TableColumn Nume;
 
-    public boolean checkForEmptyFields()
-    {
-        if ((nume_proprietate.getText().equals(""))||(author_name.getText().equals("")))
-            return true ;
-        return false ;
+
+    public void populateTableReviews() {
+        //System.out.println(rez.get(0).getLocatie());
+        //getData();
+        ObservableList<Review> data2 = table.getItems();
+        //ObservableList<Integer> dat = tableView.getItems();
+        //for (int j = 0 ; j < rez.size() ; j ++ )
+        {
+            //if ((rez.get(j).getFull_name()).equals(this.ClientName))
+                for (int i = 0; i < reviewsList.size(); i++) {
+                    //if (((reviewsList.get(i).getNume_proprietate()) != null)&&((reviewsList.get(i).getNume_proprietate()).equals(results.get(j).getNume_proprietate())))
+                        data2.add(new Review(reviewsList.get(i).getNume_proprietate(),
+                                            reviewsList.get(i).getNume_autor(),
+                                            reviewsList.get(i).getText_review(),
+                                            reviewsList.get(i).getDate()
+                                )
+                                
+                        );
+                }
+        }
+        /*
+        table.setRowFactory(param -> {
+            return new TableRow() {
+                @Override
+                public void updateIndex(int i) {
+                    super.updateIndex(i);
+
+                    setMinHeight(50 * i);
+                }
+            };
+        });
+         */
+        final Callback<TableColumn<Review,String>, TableCell<Review,String>> WRAPPING_CELL_FACTORY =
+                new Callback<TableColumn<Review,String>, TableCell<Review,String>>() {
+
+                    @Override public TableCell<Review,String> call(TableColumn<Review,String> param) {
+                        TableCell<Review,String> tableCell = new TableCell<Review,String>() {
+                            @Override protected void updateItem(String item, boolean empty) {
+                                if (item == getItem()) return;
+
+                                super.updateItem(item, empty);
+
+                                if (item == null) {
+                                    super.setText(null);
+                                    super.setGraphic(null);
+                                } else {
+                                    super.setText(null);
+                                    Label l = new Label(item);
+                                    l.setWrapText(true);
+                                    VBox box = new VBox(l);
+                                    l.heightProperty().addListener((observable,oldValue,newValue)-> {
+                                        box.setPrefHeight(newValue.doubleValue()+7);
+                                        Platform.runLater(()->this.getTableRow().requestLayout());
+                                    });
+                                    super.setGraphic(box);
+                                }
+                            }
+                        };
+                        return tableCell;
+                    }
+                };
+
+        textReview.setCellFactory(tc -> {
+            TableCell<Review, String> cell = new TableCell<>();
+            Text text = new Text();
+            cell.setGraphic(text);
+            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+            text.wrappingWidthProperty().bind(textReview.widthProperty());
+            text.textProperty().bind(cell.itemProperty());
+            return cell ;
+        });
+
+        Nr.setCellFactory(col -> new TableCell<Task, String>() {
+            @Override
+            public void updateIndex(int index) {
+                super.updateIndex(index);
+                if (isEmpty() || index < 0) {
+                    setText(null);
+                } else {
+                    setText(Integer.toString(index+1));
+                }
+            }
+        });
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        populateTableReviews();
+    }
 
 }
